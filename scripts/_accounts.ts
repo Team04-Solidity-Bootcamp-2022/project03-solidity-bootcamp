53de5f3b82f5dcf124caab939a9a44b8652e36cc
franciscom-bootcamp <encode@mail.fmf.rocks>
2022-10-09 17:34:46 +0100

    Refactoring accounts and removing hardcoded private keys

--- scripts/_accounts.ts
+++ scripts/_accounts.ts
@@ -1,11 +1,10 @@
 import { ethers } from "ethers";
 
 export async function getSignerArray(): Promise<ethers.Wallet[]> {
-    const provider = ethers.getDefaultProvider("http://127.0.0.1:8545/");
-    const deployer = new ethers.Wallet("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80");
-    const acc1 = new ethers.Wallet("0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d");
-    const acc2 = new ethers.Wallet("0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a");
-    //const acc2 = new ethers.Wallet(process.env.PRIVATE_KEY_3 ?? "");
+    const provider = ethers.getDefaultProvider("goerli");
+    const deployer = new ethers.Wallet(process.env.PRIVATE_KEY_1 ?? "");
+    const acc1 = new ethers.Wallet(process.env.PRIVATE_KEY_2 ?? "");
+    const acc2 = new ethers.Wallet(process.env.PRIVATE_KEY_3 ?? "");
   
     console.log(`Using addresses:\n${deployer.address}\n${acc1.address}\n${acc2.address}`);
     
