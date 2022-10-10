fe58fea809c50c68f3d25337281e173dfe7e3008
franciscom-bootcamp <encode@mail.fmf.rocks>
2022-10-10 17:41:40 +0100

    Refactoring CheckVotingPower

--- scripts/CheckVotingPower.ts
+++ scripts/CheckVotingPower.ts
@@ -1,6 +1,9 @@
 // IMPORTS
 import { getContract } from "./DeployToken";
 import { BigNumber } from "ethers";
+import * as dotenv from "dotenv";
+dotenv.config();
+
 
 async function checkVotingPower(address: string): Promise<BigNumber> {
   const contract = await getContract();
@@ -9,4 +12,15 @@ async function checkVotingPower(address: string): Promise<BigNumber> {
   console.log({votingPower});
 
   return votingPower;
+}
+
+async function main() {
+  checkVotingPower(process.env.ROB_ADDRESS)
+}
+
+if (require.main === module) {
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});  
 }
