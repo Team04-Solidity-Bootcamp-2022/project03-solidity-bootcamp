76a27c8aec9bd7f7eddf6402c3c340a83b52ec8f
Patrick Asare <8025627+patrickAsare@users.noreply.github.com>
2022-10-10 16:54:53 +0100

    Deploy scripts

---
+++ scripts/patrick/Deploy.ts
@@ -0,0 +1,15 @@
+import { ethers } from "ethers";
+import { deployVotes, deployBallot } from "./_deploy";
+
+async function main() {
+  const votesContract = await deployVotes();
+  console.log(votesContract.address);
+
+  const ballotContract = await deployBallot(votesContract);
+  console.log(ballotContract.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
